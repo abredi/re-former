3107d4cb0a8e89bd8ad04d928361acbd330c059e
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-08-20 18:02:43 +0300

    add user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
     @user = User.new(user_params)
