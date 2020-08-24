ce0fb3633905c549d0907c4ad33d9606a19c4585
Elias Castañeda <e71az@icloud.com>
2020-08-24 15:25:03 -0600

    Created the update method for the user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  # require 'UsersHelper'
+
   def new
     @user = User.new
   end
@@ -18,10 +20,27 @@ class UsersController < ApplicationController
   def edit
     puts params[:id]
     @user = User.find(params[:id])
+    puts '**************************************'
+    puts '**************************************'
+    puts '**************************************'
+    puts '**************************************'
+    puts '************Hello**************************'
+    puts '**************************************'
+    puts '**************************************'
+    puts '**************************************'
+    puts '**************************************'
+    puts '**************************************'
+
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    flash.notice = "User '#{@user.username}' Updated!"
+    redirect_to users_path
   end
 
   private
-
   def user_params
     params.require(:user).permit(:email, :username, :password, :password_confirmation)
   end
