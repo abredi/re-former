8828942991cef9609fed612d2c4f4a4de0561e34
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-08-24 23:24:48 +0300

    update the add new user feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,23 +8,28 @@ class UsersController < ApplicationController
   
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
-
   def create
-    # @user = User.new(username: params[:username], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
     @user = User.new(user_params)
     if @user.save
       redirect_to new_user_path
-    else  
-      render :new
+    else
+      render :new 
     end
   end
 
-  private
+  # def create
+  #   @user = User.new(username: params[:username], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
 
+  #   if @user.save
+  #     redirect_to new_user_path
+  #   else
+  #     render :new
+  #   end
+  # end
+
+  private
   def user_params
     params.require(:user).permit(:email, :username, :password, :password_confirmation)
   end
+
 end
