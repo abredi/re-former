1763398107a1910a5dbaee41bbe6e82ce5fe2199
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-08-25 00:38:16 +0300

    update index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,16 @@ class UsersController < ApplicationController
   end
 
   def index
-  
+    @users = User.all
+  end
+
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def create
@@ -15,6 +24,14 @@ class UsersController < ApplicationController
     else
       render :new 
     end
+  end
+
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    flash.notice = "User '#{@user.username}' Updated!"
+    redirect_to users_path
   end
 
   # def create
