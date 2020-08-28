b84bb9e8fda1949779a47fe1622b902f8e69896a
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-08-28 14:41:20 +0300

    update index page ui

--- Gemfile
+++ Gemfile
@@ -35,7 +35,7 @@ group :production do
 end
 
 group :development, :test do
-# Use sqlite3 as the database for Active Record
+  # Use sqlite3 as the database for Active Record
   gem 'sqlite3', '~> 1.4'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :find_post, only: [:show, :update, :edit, :destroy]
+
   def new
     @action = 'Create New'
     @user = User.new
@@ -9,12 +11,12 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end
 
   def edit
     @action = 'Update'
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end
 
   def create
@@ -28,9 +30,14 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     @user.update(user_params)
     flash.notice = "User '#{@user.username}' Updated!"
+    redirect_to users_path
+  end
+
+  def destroy
+    @user.destroy
+    flash.notice = "User deleted!"
     redirect_to users_path
   end
 
@@ -38,5 +45,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:email, :username, :password, :password_confirmation)
+  end
+
+  def find_post
+    @user = User.find(params[:id])
   end
 end
